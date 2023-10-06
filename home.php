c9079723d6fcf87834e5b29d648f23a4b342e99b
nasraismail33 <nasraismail274@gmail.com>
2023-10-07 00:30:34 +0300

    modified:home.php and adding new photo

--- home.php
+++ home.php
@@ -3,7 +3,69 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300&display=swap" rel="stylesheet">
     <title>student</title>
+    <style>
+        body{
+            background-color: whitesmoke;
+            font-family: 'Tajawal', sans-serif;
+        }
+        #mother{
+            width: 100%;
+            font-size: 20px;
+        }
+        main{
+            float: right;
+            border: 1px solid gray;
+            padding: 5px;
+        }
+        input{
+            padding:4px;
+            border: 2px solid black;
+            text-align:center;
+            font-size: 17px;
+            font-family: 'Tajawal', sans-serif;
+        }
+        aside{
+            text-align:center;
+            width:300px;
+            float:left;
+            border: 1px solid black;
+            padding:10px;
+            font-size:20px;
+            background-color: silver;
+            color:white;
+        }
+        #tbl{
+            width:890px;
+            font-size:20px;
+        }
+        #tbl th{
+            background-color: silver;
+            color: #000;
+            font-size: 20px;
+            padding: 10px;
+        }
+        aside button{
+            width: 190px;
+            padding: 8px;
+            margin-top: 7px;
+            font-size: 17px;
+            font-family: 'Tajawal', sans-serif;
+            
+
+
+        }
+
+
+
+
+    </style>
+
+
+
 </head>
 <body>
     
@@ -12,7 +74,7 @@
         <!-- لوحة التعلم -->
         <aside>
             <div id="div">
-                <img src="" alt="لوجو الموقع">
+                <img src="student.png" alt="لوجو الموقع" width="200">
                 <h3>لوحة المدير</h3>
                 <label for="">رقم الطالب</label><br>
                 <input type="text" name="id" id="id"><br>
@@ -25,6 +87,7 @@
                 <button name="del">حذف الطالب</button>
             </div>
         </aside>
+
 
        <!-- عرض بيانات الطلاب -->
         <main>
